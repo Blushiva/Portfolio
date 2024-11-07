1e14bb5749926536dceb6700e14a2533355fde02
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-11-07 11:56:05 -0600

    added class names

--- portfolio/src/components navbar/navbar.js
+++ portfolio/src/components navbar/navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import styles from "portfolio/src/components navbar/navbar.module.css"
+
 export const Navbar = () => {
     return (
-    <nav>
-        <a href="/">Portfolio</a>
-        <div>
-            <ul>
-
+    <nav className= {StyleSheet.Navbar}>
+        <a className={styles.title}
+        href="/">Portfolio
+        </a>
+        <div className= {styles.menu}>
+            <ul className= {styles.menuItems}>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experiance</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </div>
     </nav>
