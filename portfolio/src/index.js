4e6a98eb99ca783a859af406ce36f1c929d9b396
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-11-07 09:24:25 -0600

    added fonts

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,9 +1,15 @@
+// imports the app components
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Importing fonts
+import "@fontsource/outfit";  
+import "@fontsource/roboto";
+
+//initializes a react app by rendering the app component into the root node
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
